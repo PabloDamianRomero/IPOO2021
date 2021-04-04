f932bfeaf7967f18fa1b9fcb282d334ff0526262
PabloDamianRomero <pablo.romero@est.fi.uncoma.edu.ar>
2021-04-03 22:37:52 -0300

    Subida Clase Funcion

---
+++ Tp 02/Ejercicio 08/Funcion.php
@@ -0,0 +1,67 @@
+<?php
+class Funcion
+{
+    private $nombreFuncion;
+    private $horaInicio;
+    private $duracion;
+    private $precioFuncion;
+
+    public function __construct($pNombre, $pInicio, $pDuracion, $pPrecio)
+    {
+        $this->nombreFuncion = $pNombre;
+        $this->horaInicio = $pInicio;
+        $this->duracion = $pDuracion;
+        $this->precioFuncion = $pPrecio;
+    }
+
+    public function getNombreFuncion()
+    {
+        return $this->nombreFuncion;
+    }
+
+    public function getHoraInicio()
+    {
+        return $this->horaInicio;
+    }
+
+    public function getDuracion()
+    {
+        return $this->duracion;
+    }
+
+    public function getPrecioFuncion()
+    {
+        return $this->precioFuncion;
+    }
+
+    public function setNombreFuncion($pNombre)
+    {
+        $this->nombreFuncion = $pNombre;
+    }
+
+    public function setHoraInicio()
+    {
+        $this->horaInicio = $pInicio;
+    }
+
+    public function setDuracion($pDuracion)
+    {
+        $this->duracion = $pDuracion;
+    }
+
+    public function setPrecioFuncion($pPrecio)
+    {
+        $this->precioFuncion = $pPrecio;
+    }
+
+    public function __toString()
+    {
+        $cadena = "\n-------------------------------------------";
+        $cadena .= "\n\t Nombre de la función: " . $this->getNombreFuncion();
+        $cadena .= "\n\t Hora de inicio: " . $this->getHoraInicio();
+        $cadena .= "\n\t Duración: " . $this->getDuracion();
+        $cadena .= "\n\t Precio: " . $this->getPrecioFuncion();
+        $cadena .= "\n-------------------------------------------";
+        return $cadena;
+    }
+}
