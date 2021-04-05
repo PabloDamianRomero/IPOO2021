21cc68e9fae05bbfab78e457d1097c2d5c259024
PabloDamianRomero <pablo.romero@est.fi.uncoma.edu.ar>
2021-04-05 03:11:14 -0300

    Comentarios añadidos y validación de funciones (Versión 2)

--- Tp 02/Ejercicio 08/TestTeatro.php
+++ Tp 02/Ejercicio 08/TestTeatro.php
@@ -22,6 +22,9 @@ function menu()
     echo "\n-------------------------------------------";
 }
 
+/**
+ * Funcion que permite crear un teatro y almacenarle cierta cantidad de funciones
+ */
 function cargarTeatro($colTeatros, $colFunciones, $objTeatro)
 {
     echo "\n Ingrese nombre de teatro: ";
@@ -32,19 +35,22 @@ function cargarTeatro($colTeatros, $colFunciones, $objTeatro)
     $cantFunciones = trim(fgets(STDIN));
     $contadorFuncion = 1;
     $anterior = 0;
-    for ($i = 0; $i < $cantFunciones; $i++) {
+    $i = 0;
+    $seguir = true;
+    while (($i < $cantFunciones) && $seguir) { // Repetitiva que permite el ingreso de funciones, hasta que lo permita el horario del teatro
         echo "\n Ingrese el nombre de la función " . $contadorFuncion . ": ";
         $nombreFuncion = trim(fgets(STDIN));
-        do {
-            echo "\n Ingrese la hora de inicio de la función " . $contadorFuncion . ": ";
+        do { // Repetitiva para pedir continuamente la hora, en caso de ser incorrecta (por función)
+            echo "\n Ingrese la hora de inicio de la función " . $contadorFuncion . " (Recuerde que debe ser 1hr después de haber terminado la función anterior o a partir de cero si es la primera): ";
             $horaInicio = trim(fgets(STDIN));
             echo "\n Cuántas hs dura la función " . $contadorFuncion . "?: ";
             $duracionFuncion = trim(fgets(STDIN));
-            $resp = verificarHorarios($anterior, $horaInicio, $duracionFuncion);
+            $resp = verificarHorarios($anterior, $horaInicio, $duracionFuncion); // verifica si la hora ingresada es valida (0 a 23hs)
             if (!$resp) {
                 echo "\n\n Deberá ingresar nuevamente los horarios. (El horario se vió pisado con el anterior)\n";
             } else {
                 $anterior = $horaInicio + $duracionFuncion;
+                $seguir = verificarUltimaHora($anterior); // si quedan funciones por ingresar pero ya se completó el horario, se utiliza este atributo como condicion de corte
             }
         } while (!$resp);
         echo "\n Ingrese el precio de la función " . $contadorFuncion . ": ";
@@ -52,6 +58,10 @@ function cargarTeatro($colTeatros, $colFunciones, $objTeatro)
         $objFuncion = new Funcion($nombreFuncion, $horaInicio, $duracionFuncion, $precioFuncion);
         $colFunciones[$i] = $objFuncion;
         $contadorFuncion++;
+        $i++;
+    }
+    if (!$seguir) {
+        echo "\nLos horarios del teatro fueron todos ocupados. No se permiten nuevas funciones. ";
     }
     $objTeatro = new Teatro($nombreTeatro, $dirTeatro, $colFunciones);
     $cantidadTeatros = count($colTeatros);
@@ -59,36 +69,55 @@ function cargarTeatro($colTeatros, $colFunciones, $objTeatro)
     return $colTeatros;
 }
 
-function cargarFunciones($colTeatros, $cant, $nroTeatro){
+
+/**
+ * Función que permite agregarle funciones a un teatro en particular (existente)
+ */
+function cargarFunciones($colTeatros, $cant, $nroTeatro)
+{
     $contadorFuncion = count($colTeatros[$nroTeatro]->getFunciones());
     $auxFunciones = $colTeatros[$nroTeatro]->getFunciones();
-    $anterior = $auxFunciones[$contadorFuncion-1]->getHoraInicio()+ $auxFunciones[$contadorFuncion-1]->getDuracion();
-    echo "ANTERIOR: ".$anterior;
-    $valorDePosicionActual = $contadorFuncion + 1;
-    for ($i = 0; $i < $cant; $i++) {
-        echo "\n Ingrese el nombre de la función " . $valorDePosicionActual . ": ";
-        $nombreFuncion = trim(fgets(STDIN));
-        do {
-            echo "\n Ingrese la hora de inicio de la función " . $valorDePosicionActual . ": ";
-            $horaInicio = trim(fgets(STDIN));
-            echo "\n Cuántas hs dura la función " . $valorDePosicionActual . "?: ";
-            $duracionFuncion = trim(fgets(STDIN));
-            $resp = verificarHorarios($anterior, $horaInicio, $duracionFuncion);
-            if (!$resp) {
-                echo "\n\n Deberá ingresar nuevamente los horarios. (El horario se vió pisado con el anterior)\n";
-            } else {
-                $anterior = $horaInicio + $duracionFuncion;
+    $anterior = $auxFunciones[$contadorFuncion - 1]->getHoraInicio() + $auxFunciones[$contadorFuncion - 1]->getDuracion();
+    $seguir = verificarUltimaHora($anterior);
+    if ($seguir) { // Verifica si la primera ejecución puede realizarse, en base al último horario de la función
+        $valorDePosicionActual = $contadorFuncion + 1;
+        $i = 0;
+        while (($i < $cant) && ($seguir)) { // Repetiva para almacenar nuevas funciones
+            echo "\n Ingrese el nombre de la función " . $valorDePosicionActual . ": ";
+            $nombreFuncion = trim(fgets(STDIN));
+            do { // Repetitiva para pedir continuamente la hora, en caso de ser incorrecta (por función)
+                echo "\n Ingrese la hora de inicio de la función " . $valorDePosicionActual . " (Recuerde que debe ser 1hr después de haber terminado la función anterior): ";
+                $horaInicio = trim(fgets(STDIN));
+                echo "\n Cuántas hs dura la función " . $valorDePosicionActual . "?: ";
+                $duracionFuncion = trim(fgets(STDIN));
+                $resp = verificarHorarios($anterior, $horaInicio, $duracionFuncion); // verifica si la hora ingresada es valida (0 a 23hs)
+                if (!$resp) {
+                    echo "\n\n Deberá ingresar nuevamente los horarios. (El horario se vió pisado con el anterior)\n";
+                } else {
+                    $anterior = $horaInicio + $duracionFuncion;
+                    $seguir = verificarUltimaHora($anterior); // si quedan funciones por ingresar pero ya se completó el horario, se utiliza este atributo como condicion de corte
+                }
+            } while (!$resp);
+            echo "\n Ingrese el precio de la función " . $valorDePosicionActual . ": ";
+            $precioFuncion = trim(fgets(STDIN));
+            $objFuncion = new Funcion($nombreFuncion, $horaInicio, $duracionFuncion, $precioFuncion);
+            $auxFunciones[$contadorFuncion] = $objFuncion;
+            $contadorFuncion++;
+            $i++;
+            if (!$seguir) { // Avisa por pantalla que a pesar de que queden funciones por ingresar, ya se completo el horario
+                echo "\nHorarios completados, ya no se permite registrar otra función";
             }
-        } while (!$resp);
-        echo "\n Ingrese el precio de la función " . $valorDePosicionActual . ": ";
-        $precioFuncion = trim(fgets(STDIN));
-        $objFuncion = new Funcion($nombreFuncion, $horaInicio, $duracionFuncion, $precioFuncion);
-        $auxFunciones[$contadorFuncion] = $objFuncion;
-        $contadorFuncion++;
+        }
+    } else {
+        echo "\nYa no hay lugar para otra función, horarios completos";
     }
     return $auxFunciones;
 }
 
+
+/**
+ * Función que verifica si el horario de una función corresponde con el ciclo de hora real de un dia (0 a 23)
+ */
 function verificarHorarios($anterior, $inicio, $fin)
 {
     $respuesta = false;
@@ -100,6 +129,20 @@ function verificarHorarios($anterior, $inicio, $fin)
     return $respuesta;
 }
 
+/**
+ * Función que verifica la hora de la última función de un teatro, especificando
+ * si es posible agregar una nueva función
+ */
+function verificarUltimaHora($valor)
+{
+    $resp = true;
+    if ($valor >= 22) {
+        $resp = false;
+    }
+    return $resp;
+}
+
+
 function main($colTeatros, $colFunciones, $objTeatro)
 {
     do {
@@ -109,7 +152,6 @@ function main($colTeatros, $colFunciones, $objTeatro)
             case 1:
                 $colTeatros = cargarTeatro($colTeatros, $colFunciones, $objTeatro);
                 break;
-
             case 2:
                 if ($colTeatros != null) {
                     $longitud = count($colTeatros);
@@ -129,14 +171,13 @@ function main($colTeatros, $colFunciones, $objTeatro)
                         }
                     }
                     if ($encontrado) {
-                        echo "\n Ingrese cantidad de funciones nuevas para teatro '".$colTeatros[$j]->getNombreTeatro()."': ";
+                        echo "\n Ingrese cantidad de funciones nuevas para teatro '" . $colTeatros[$j]->getNombreTeatro() . "': ";
                         $cantFunciones = trim(fgets(STDIN));
-                        $tmpFuncion = cargarFunciones($colTeatros,$cantFunciones, $j);
+                        $tmpFuncion = cargarFunciones($colTeatros, $cantFunciones, $j);
                         $colTeatros[$j]->setFunciones($tmpFuncion);
                     } else {
                         echo "\nNo existe teatro con ese nombre";
                     }
-                    // ------------------
                 } else {
                     echo "\n No existen teatros";
                 }
